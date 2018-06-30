d295bb50b3f8b7982ddbcde65cd9d223dc93e6e4
ishowta <kinironote@gmail.com>
2018-06-30 20:06:58 +0900

    add relations and access controls

--- common/models/feed.js
+++ common/models/feed.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(Feed) {
-
+  Feed.beforeRemote('create', function(context, user, next) {
+    context.args.data.ownerId = context.req.accessToken.userId;
+  });
 };
--- server/boot/automigrate.js
+++ server/boot/automigrate.js
@@ -3,7 +3,7 @@
 module.exports = function(app) {
   var db = app.dataSources.db;
   var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role',
-    'Account', 'Feed', 'AccountFeed'];
+    'Account', 'Feed'];
   db.autoupdate(lbTables, function(er) {
     if (er) throw er;
     console.log(
