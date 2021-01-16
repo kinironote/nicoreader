3afef89a46bad6fcdea4c48df613d2a0b2c3f8ee
ishowta <ishowta@gmail.com>
2021-01-17 00:20:15 +0900

    style: update auth form

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -106,6 +106,7 @@ const Header = ({ login, signup, logout, isLoggedIn }: HeaderPropType) => {
                 }}
               />
               <br />
+              <br />
               <Button type="submit">ログイン</Button>
               <Button onClick={() => setOnLogin(false)}>閉じる</Button>
             </form>
